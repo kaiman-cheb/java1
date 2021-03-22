9a87dc06057f73c72d70e089efeb0d8427b66956
kaiman <eayu@ya.ru>
2021-03-22 20:27:35 +0300

    H6 HOMEWORK-1

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -6,18 +6,15 @@ public class Eratosthenes {
     private boolean[] sieve;
     int N;
     int p;
-       public Eratosthenes(int N) {
+       public Eratosthenes(int N)
+    {
         this.N = N;
-//       }
     sieve = new boolean[N];
-//       {
-//        Arrays.fill(sieve, true);
+
         for (int k=0; k<(N); k++)
         {
             sieve[k] = true;
-//            System.out.print(k);
         }
-//           System.out.println(Arrays.toString(sieve));
            sift();
     }
     private void sift (){
@@ -25,11 +22,12 @@ public class Eratosthenes {
         sieve[1] = false;
        for (int i=2; i< sieve.length; i++)
         {
-//            System.out.print(i + "-");
-//            System.out.println(sieve[i]);
-                for (int j=2; j<(sieve.length/i); j++) {
+
+                for (int j=2; j<=(sieve.length/i); j++) {
                     p = j * i;
-                    sieve[p] = false;
+                    if ((p+1) <= sieve.length) {
+                        sieve[p] = false;
+                    }
                 }
         }
 //        System.out.println(N);
@@ -40,9 +38,9 @@ public class Eratosthenes {
         }
 
         public static void main (String[]args){
-            Eratosthenes re6eto = new Eratosthenes(14);
+            Eratosthenes re6eto = new Eratosthenes(300);
             System.out.println(Arrays.toString(re6eto.sieve));
-            System.out.println(re6eto.isSimple(13));
+            System.out.println(re6eto.isSimple(299));
         }
     }
 
